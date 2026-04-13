ca739fd067d1cf7715318dfa6d9d02765c947654
akulanga <akulanga@COMPSCI-FAC>
2026-04-13 15:28:25 -0500

    Change from Ashwin

--- Hannah_Lab1/Hannah_Lab1.cpp
+++ Hannah_Lab1/Hannah_Lab1.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello from Hannah! This is my first Git commit >:)";
+	cout << "Hello from Ashwin! This is my first Git commit";
 	cout << "Git makes collaboration easy! " << endl;
 
 	cout << "Lab 1 completed on [04/13/26]" << endl;
